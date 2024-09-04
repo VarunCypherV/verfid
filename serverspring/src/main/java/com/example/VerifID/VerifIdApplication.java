58d01ce775a71787d6b4186504ca90c998b53b2a
VarunCypherV <varunvk2004@gmail.com>
2024-09-04 20:10:20 +0530

    project base

---
+++ serverspring/src/main/java/com/example/VerifID/VerifIdApplication.java
@@ -0,0 +1,13 @@
+package com.example.VerifID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifIdApplication.class, args);
+	}
+
+}
---
+++ serverspring/src/test/java/com/example/VerifID/VerifIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VerifID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
