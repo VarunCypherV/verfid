89f0e584566ef9c51b0ec60d3ecf9b401b9907de
VarunCypherV <varunvk2004@gmail.com>
2024-10-03 16:05:25 +0530

    nav and footer

--- clientnext/src/app/components/footer.js
+++ clientnext/src/app/components/footer.js
@@ -2,24 +2,35 @@
 
 import Link from "next/link";
 import React from "react";
+import Subscribe from "./subscribe";
 
 function Footer() {
   return (
-    <div className="Nav">
-      <div className="Left">
+    <div className="Footer">
+      <div className="F_Left">
         <img src="/Assets/Logo.jpg"/>
-      </div>
-      <div className="Right">
         <div>
-            <img src="/Assets/LightSwitch.png"/>
+            <p>VerifID Headquarters</p>
+            <p>#27, Taramani Road,</p>
+            <p>Saidapet, Chennai - 96</p>
         </div>
-        <Link href="/goals" className="nav-link">Goals</Link>
-        <Link href="/goals" className="nav-link">Workflow</Link>
-        <Link href="/goals" className="nav-link">Contact Us</Link>
-        <Link href="/goals" className="nav-link">Your Identity</Link>
-        <Link href="" className="ProfileImg">
-            <img  src="/Assets/Profile.png"/>
-        </Link>
+        <div>
+            <p>Copyright © 2024 All Rights Reserved</p>
+        </div>
+      </div>
+
+      <div className="F_Middle">
+        <h3>CONTACT US</h3>
+
+        <p>T: +91 123 456 789</p>
+        <p>E: verifID@gmail.com</p>
+      </div>
+      <div className="F_Right">
+        <h3>NEWSLETTER</h3>
+
+        <p>Want to keep your digital identity secure and verified?</p>
+        <p>Sign up to stay informed and access updates, insights!</p>
+        <Subscribe/>
       </div>
     </div>
   );
---
+++ clientnext/src/app/components/subscribe.js
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+
+function Subscribe() {
+  return (
+    <div className="sub-container">
+      <div className="input-section">
+        <input type="email" placeholder="Email address" className="email-input" />
+      </div>
+      <div className="button-section">
+        <button className="subscribe-button">Subscribe</button>
+      </div>
+    </div>
+  );
+}
+
+export default Subscribe;
