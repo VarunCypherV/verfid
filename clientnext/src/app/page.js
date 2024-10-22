b052de3824afe68cf37334cf4982041598bff6a8
Varun Vetrivendan <91377728+VarunCypherV@users.noreply.github.com>
2024-10-22 22:38:00 +0530

    Update page.js

--- clientnext/src/app/page.js
+++ clientnext/src/app/page.js
@@ -1,19 +1,11 @@
 "use client";
 
-import Image from "next/image";
-import styles from "./page.module.css";
 import Land from "./Land/page";
-import Register from "./Register/page";
-import Profile from "./Profile/page";
-import Login from "./Login/page";
 
 export default function Home() {
   return (
     <div>
       <Land />
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <Profile /> */}
     </div>
   );
 }
